d4e002aed6aed6427e7de569d8f5f6a8cbeda0ca
Mikhail Bogomazov <ihackit595@gmail.com>
2022-05-05 12:31:57 +0300

    update demo

--- demo/script.js
+++ demo/script.js
@@ -1,7 +1,10 @@
-import {createElement, init} from '../dist/index.js'
-const img = createElement('img', {
-    src: 'https://seeklogo.com/images/T/tinkoff-logo-BE406CDE9D-seeklogo.com.png',
-    alt: 'Tinkoff Logo',
+import {createNode, init} from '../dist/index'
+const img = createNode('img', {
+    src: 'https://images.clipartlogo.com/files/images/44/441434/bananas-icon-clip-art_p.jpg',
+    alt: 'Banana Image',
 })
-const a = createElement('a', {href: 'https://tinkoff.ru'}, img)
-init(a, '#app')
+const a = createNode('a', {href: 'https://google.com'}, img)
+const h2 = createNode('h2', {}, 'BANANA!!!')
+const div = createNode('div', {}, h2)
+const mainDiv = createNode('div', {}, a, div)
+init(mainDiv, '#app')
